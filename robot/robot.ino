8bfc65208c4a4089fff0c76cbd2a61e2dad54c00
joaofabricio <jfabri2@gmail.com>
2013-05-12 15:46:27 -0300

    Som de ré (speaker)

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,32 +1,36 @@
 #include <Servo.h>
 
+const int DELAY = DELAY;
+
 const int SERVO = 2;
 const int CC = 3;
 const int CC_REVERSE = 4;
+const int SPEAKER = 5;
 
-const int FRENTE = 49;//comando
-const int RE = 50;//comando
-const int DIREITA = 51;//comando
-const int ESQUERDA = 52;//comando
+const int FORWARD = 49;//comando "1"
+const int BACKWARD = 50;//comando "2"
+const int RIGHT = 51;//comando "3"
+const int LEFT = 52;//comando "4"
 
-const int ANGULO_MEIO = 90;
-const int ANGULO_DIREITA = 135;
-const int ANGULO_ESQUERDA = 45;
+const int ANGLE_FRONT = 90;
+const int ANGLE_RIGHT = 135;
+const int ANGLE_LEFT = 45;
 
-const int DIR_MEIO = 0;
-const int DIR_ESQUERDA = 1;
-const int DIR_DIREITA = 2;
+const int DIR_FRONT = 0;
+const int DIR_LEFT = 1;
+const int DIR_RIGHT = 2;
 
-int direcao = DIR_MEIO;
+int dir = DIR_FRONT;
 
 Servo servoMotor;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(SPEAKER, OUTPUT);
   pinMode(CC, OUTPUT);
   pinMode(CC_REVERSE, OUTPUT);
   servoMotor.attach(SERVO);      // conecta o motor ao pino de controle
-  servoMotor.write(ANGULO_MEIO);      // gira o servo ate o ponto do meio
+  servoMotor.write(ANGLE_FRONT);      // gira o servo ate o ponto do meio
 }
 
 int incoming = 0;
@@ -36,40 +40,45 @@ void loop() {
     incoming = Serial.read();
     
     switch (incoming) {
-      case FRENTE:
+      case FORWARD:
         digitalWrite(CC_REVERSE, LOW);
         digitalWrite(CC, HIGH);
+        delay(DELAY);
       break;
       
-      case RE:
+      case BACKWARD:
         digitalWrite(CC, LOW);
         digitalWrite(CC_REVERSE, HIGH);
+        delay(DELAY/2);
+        digitalWrite(SPEAKER, HIGH);
+        delay(DELAY/2);
+        digitalWrite(SPEAKER, LOW);
       break;
       
-      case DIREITA:
-        if (direcao == DIR_MEIO) {
-          servoMotor.write(ANGULO_DIREITA);
-          direcao = DIR_DIREITA;
-        } else if (direcao == DIR_ESQUERDA) {
-          servoMotor.write(ANGULO_MEIO);
-          direcao = DIR_MEIO;
+      case RIGHT:
+        if (dir == DIR_FRONT) {
+          servoMotor.write(ANGLE_RIGHT);
+          dir = DIR_RIGHT;
+        } else if (dir == DIR_LEFT) {
+          servoMotor.write(ANGLE_FRONT);
+          dir = DIR_FRONT;
         }
+        delay(DELAY);
       break;
       
-      case ESQUERDA:
-        if (direcao == DIR_MEIO) {
-          servoMotor.write(ANGULO_ESQUERDA);
-          direcao = DIR_ESQUERDA;
-        } else if (direcao == DIR_DIREITA) {
-          servoMotor.write(ANGULO_MEIO);
-          direcao = DIR_MEIO;
+      case LEFT:
+        if (dir == DIR_FRONT) {
+          servoMotor.write(ANGLE_LEFT);
+          dir = DIR_LEFT;
+        } else if (dir == DIR_RIGHT) {
+          servoMotor.write(ANGLE_FRONT);
+          dir = DIR_FRONT;
         }
+        delay(DELAY);
       break;
       
     }
-    
-    delay(500);
-    
+        
     digitalWrite(CC, LOW);
     digitalWrite(CC_REVERSE, LOW);
     
